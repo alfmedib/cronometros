7d5975c6ce2dc250ba9a61038286085a0e5b1af3
Alfonso Médicis <alfmedis@gmail.com>
2018-08-29 15:56:34 -0500

    corregir variables privadas de cronometros

--- src/app/componentes/crodesfibrilar/crodesfibrilar.component.ts
+++ src/app/componentes/crodesfibrilar/crodesfibrilar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Reloj} from '../../modelo/reloj';
 
 @Component({
   selector: 'app-crodesfibrilar',
@@ -7,95 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrodesfibrilarComponent implements OnInit {
 
-  public hora: number = 0;
-  public minutos: number = 0;
-  public segundos: number = 0;
-  public contador: number = 0; // este cuenta las desfibrilaciones
-  public cont:any; // contador del reloj
   public message: string = '';
   public porcentaje:number = 0;
+  public relojDesfibrilador: Reloj = new Reloj();
+  public desfibrilador: any;
+  public contador: number = 0;
 
-  public horaLapso: number = 0;
-  public minutosLapso: number = 0;
-  public segundosLapso: number = 0;
-  public contadorLapso: number = 0;
-  public messageLapso: string = '';
-  public porcentajeLapso:number = 0;
-
-
-  constructor() { }
+ 
+  constructor() { 
+    this.relojDesfibrilador.resetTimer();
+  }
 
   ngOnInit() {
   }
 
   start(){
-      /*this.segundos = 0;
-      this.minutos = 0;
-      this.hora = 0;
-      this.porcentaje = 0;*/
-    if(this.cont == undefined){
-     this.cont = setInterval(() => {
-        this.segundos++;
-        this.porcentaje += 0.83333; 
+    clearInterval(this.desfibrilador);
+    this.desfibrilador = setInterval(() => {
+
+      this.relojDesfibrilador.getStart();
+      this.porcentaje += 0.83333; 
         if(this.porcentaje >= 100){
           this.porcentaje=0;
         }
-        if(this.segundos == 60){
-          this.segundos = 0
-          this.minutos++;
-          if(this.minutos == 60){
-            this.minutos = 0;
-            this.hora++;
-            if(this.hora == 24){
-              this.hora = 0;
-            }
-          }
-        }
-        
-      },1000
-  );
-    }
-  }
+    }, 1000);
 
-  stop(){
-    clearInterval(this.cont);
-    this.cont = null;
-  }
-
-  continuar(){
-    if(this.hora !=0 || this.minutos !=0 || this.segundos != 0){
-      if(this.cont == undefined){
-        this.cont = setInterval(() => {
-          this.segundos++;
-          this.porcentaje += 0.83333; 
-          if(this.porcentaje >= 100){
-            this.porcentaje=0;
-          }
-          if(this.segundos == 60){
-            this.segundos = 0
-            this.minutos++;
-            if(this.minutos == 60){
-              this.minutos = 0;
-              this.hora++;
-              if(this.hora == 24){
-                this.hora = 0;
-              }
-            }
-          }
-          
-        },1000);
-    }
-    }
-
-  }
-
-  lapso(){
-    this.horaLapso = this.hora;
-    this.minutosLapso = this.minutos;
-    this.segundosLapso = this.segundos;
-  }
-
-  contDesfibrilacion(){
     this.contador++;
     if(this.contador == 0 || this.contador > 3){
       this.message = '';
@@ -112,23 +49,23 @@ export class CrodesfibrilarComponent implements OnInit {
     if(this.contador == 3){
       this.message = 'Considere administrar AMIODARONA 300 mg, Tratar las causas reversibles';
     }
+  }
 
+  stop(){
+    clearInterval(this.desfibrilador);
   }
 
   reiniciar(){
-
-    clearInterval(this.cont);
-    this.cont = null;
-    this.contador = 0;
-    this.hora = 0;
-    this.minutos = 0;
-    this.segundos = 0;
+    clearInterval(this.desfibrilador);
+    this.relojDesfibrilador.resetTimer();
     this.porcentaje = 0;
-    this.message = '';
+    this.contador = 0;
   }
 
   setwidth(){
     return this.porcentaje + '%';
   }
+
+ 
 
 }
